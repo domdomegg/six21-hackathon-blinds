6e32b9939389c7106aee05471d42a0f90d8b8199
Adam Jones <jones_adam@rocketmail.com>
2018-02-05 22:28:04 +0000

    Update wemoserver.js

--- wemoserver.js
+++ wemoserver.js
@@ -1,20 +1,11 @@
 'use strict';
 
-// Change this to your local IP - this may change every time you connect to Wi-Fi
-// You can find this out by alt-clicking the Wifi icon on a Mac and looking at "IP address"
-// It should probably start with 192 or 172 - if it doesn't it might be your external one
-// If this works then it's fine, it won't do any harm to use your external IP
+// Change this to your local IP
 const YOUR_IP = '192.168.1.1';
 
 // Import Fauxmojs and Robotjs
-// Fauxmojs handles Wemo emulation, so Alexa can detect our comptuter and communicate with it
-// Robotjs controls your computer, so when Alexa asks something it can type and send something in the Arduino Serial Monitor
 const FauxMo = require('fauxmojs');
 const robot = require("robotjs");
-// NB: You must have these in the node_modules folder
-// To install, do:
-//		npm install fauxmojs
-//		npm install robotjs
 
 // Create fake Wemo device
 let fauxMo = new FauxMo({
